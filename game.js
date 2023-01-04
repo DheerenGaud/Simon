143fd3f85f9fa1178f12b490fcd4faa13c1803dc
Dheeren <110242985+DheerenGaud@users.noreply.github.com>
2023-01-04 16:18:51 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,83 @@
+let userClickedPattern=[];
+let level=0;
+var started = false;
+let buttonColours=["red","blue","green","yellow"]
+let gamePattern=[];
+let randomNumber;
+let randomChosenColour;
+
+
+$(".rule").slideUp();
+function nextSequence(){
+    let n=Math.random();
+    n=n*4;
+    randomNumber=Math.floor(n);
+    randomChosenColour=buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    var crash = new Audio("./sounds/"+randomChosenColour+".mp3");
+    crash.play();
+    level++;
+    if(level!==0){
+        $("h1").text("Level "+level);
+    }
+}
+
+$(".btn").click(function(){
+   let userChosenColour=this.id
+   userClickedPattern.push(userChosenColour+"");
+   console.log(userClickedPattern)
+   playSound(userChosenColour);
+   animatePress(userChosenColour);
+   checkAnswer(userClickedPattern.length-1)
+})
+
+function playSound(name){
+    var crash = new Audio("./sounds/"+name+".mp3");
+    crash.play();
+}
+function animatePress(animatePress){
+       $("#"+animatePress).addClass("pressed");
+       setTimeout(()=>{
+           $("#"+animatePress).removeClass("pressed");
+       },100)
+}
+
+$(document).keypress(function() {
+    if (!started) {
+          level=0;
+    $("body").removeClass("game-over")
+      $("#level-title").text("Level " + level);
+      nextSequence();
+      started = true;
+    }
+  });
+function checkAnswer(currentLevel){
+   if(userClickedPattern[currentLevel]===gamePattern[currentLevel]){
+    console.log("succes")
+    if(userClickedPattern.length===gamePattern.length){
+        setTimeout(function () {
+            userClickedPattern = [];
+            nextSequence();
+          }, 1000);
+    }
+   }
+   else{
+    console.log("wrong")
+     playSound("wrong")
+     reStart();
+     setTimeout(function () {
+        $("body").addClass("game-over")
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+      }, 200);
+   }
+}
+function reStart(){
+
+    userClickedPattern = [];
+    started=false;
+}
+
+$(".Rbtn").click(function(){
+    $(".rule").slideToggle();
+})
